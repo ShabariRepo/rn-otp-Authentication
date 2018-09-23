e131ac10baf4ff9a47bf1d5e3959cbe8cdf02f8f
shabari shenoy <sss_shenoy06@hotmail.com>
2018-09-23 17:15:01 -0400

    old vs new syntax

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -3,15 +3,38 @@ import { View, Text } from "react-native";
 import { FormLabel, FormInput, Button } from "react-native-elements";
 
 class SignupForm extends Component {
+    // old syntax
+    // constructor(props){
+    //     super(props);
+
+    //     this.state = { phone: '' };
+    // }
+    // es7 2016/17 new syntax
+    // same as this.state so you can access this below as well
+    state = { phone: '' };
+
+    // handleSubmit(){
+
+    // }
+    // above is traditional for the handler but below is new syntax
+    // but with above you have to call bind(this) in button
+    handleSubmit = () => {
+
+    }
+
     render() {
         return (
             <View>
                 <View style={{ marginBottom: 10 }}>
                     <FormLabel>Enter Phone Number</FormLabel>
-                    <FormInput />
+                    <FormInput
+                        value={ this.state.phone }
+                        onChangeText={ phone => this.setState({ phone }) }
+                        // above same as phone: phone
+                    />
                 </View>
                 
-                <Button title="Submit" />
+                <Button onPress={ this.handleSubmit.bind(this) } title="Submit" />
             </View>
         );
     }
